b90720ac4015ba57dbcb0cf0ebf645413e490283
Kaur Matthias Raavel <kmr@mrt.ee>
2025-10-01 20:34:20 +0300

    refactor: move loadConfig from main to config

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,7 +2,11 @@ package config
 
 import (
 	"errors"
+	"fmt"
 	"os"
+	"strings"
+
+	"github.com/spf13/viper"
 )
 
 type Config struct {
@@ -28,4 +32,23 @@ func (c *Config) GetDSN() (string, error) {
 		" sslmode=" + c.Database.Sslmode
 
 	return dsn, nil
+}
+
+func LoadConfig() (*Config, error) {
+	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	viper.AutomaticEnv()
+	viper.AddConfigPath("config")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+	viper.AddConfigPath(".")
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("error reading config file: %w", err)
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("unable to decode config into struct: %w", err)
+	}
+	return &cfg, nil
 }
